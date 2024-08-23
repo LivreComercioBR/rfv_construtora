77d8acc7aa2d2bce40920295852cb496b308ce9a
Ronaldo Correia <ronaldo312006@hotmail.com>
2024-08-22 23:18:48 -0300

    retirados os htmls que não estão sendo usados e incluido o path da homepage com a função lambda

--- construtora_pro/urls.py
+++ construtora_pro/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
@@ -23,5 +24,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('consultora_app.urls')),
+    path('', lambda request: redirect('homepage/'))
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
